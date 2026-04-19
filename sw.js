bb719e9baf1d2e114987e8a59f91bd997f31aa86
Exotal <74911711+Exotal@users.noreply.github.com>
2026-04-19 21:41:27 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = 'focus-timer-v1';
 const ASSETS = [
-  'timer.html',
+  'index.html',
   'manifest.json',
   'https://fonts.googleapis.com/css?family=Lato:400,700',
   'https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@600&display=swap',
@@ -19,6 +19,21 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Clean up old caches during activation
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
